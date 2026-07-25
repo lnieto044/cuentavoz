fdbcee28886c2ee369469773bccb34c6ecd150c5
Luis Nieto <guillermonieto.2003@gmail.com>
2026-07-25 13:34:25 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: { host: true, port: 5173 },
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    allowedHosts: [
+      "cuentavoz.onrender.com",
+    ],
+  },
 });
 
