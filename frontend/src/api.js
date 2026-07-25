32cf93bedd210b55b7fc3f2786053c3941bd0431
Luis Nieto <guillermonieto.2003@gmail.com>
2026-07-25 13:33:06 -0500

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-export const BASE = import.meta.env.VITE_API_URL || "http://localhost:8000" || "https://cuentavoz-api.onrender.com";
+export const BASE = import.meta.env.VITE_API_URL ||  "https://cuentavoz-api.onrender.com";
 
 
 export function guardarToken(t) {
